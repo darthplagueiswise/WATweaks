9ad704a69f338b8f6843612756226224a861ab14
euoradan <zelihmannradan@gmail.com>
2026-05-23 02:32:53 -0300

    Add WATweaks JSON backup header

---
+++ src/Menu/WAGRSettingsBackup.h
@@ -0,0 +1,14 @@
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface WAGRSettingsBackup : NSObject
+
++ (void)presentExport;
++ (void)presentImport;
++ (void)presentReset;
+
+@end
+
+NS_ASSUME_NONNULL_END
