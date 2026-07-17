0e399281da5e4bb0922cee507520039d85301881
euoradan <zelihmannradan@gmail.com>
2026-07-17 05:56:07 -0300

    dogfood2: define typed WAAB runtime scanner API

---
+++ src/Runtime/WAGRABPropsRuntime.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface WAGRABPropEntry : NSObject
+@property(nonatomic, copy) NSString *className;
+@property(nonatomic, copy) NSString *selectorName;
+@property(nonatomic, copy) NSString *typeCode;
+@property(nonatomic, copy) NSString *typeName;
+@property(nonatomic, assign) BOOL classMethod;
+@end
+
+NSArray *WAGRABPropsResolveRuntimeObjects(nullable id userContext);
+NSArray<WAGRABPropEntry *> *WAGRABPropsScan(NSArray *runtimeObjects);
+BOOL WAGRABPropEntryIsBoolean(WAGRABPropEntry *entry);
+NSString *WAGRABPropsCurrentValue(WAGRABPropEntry *entry,
+                                  NSArray *runtimeObjects,
+                                  BOOL * _Nullable boolValue,
+                                  BOOL * _Nullable boolKnown);
+
+NS_ASSUME_NONNULL_END
