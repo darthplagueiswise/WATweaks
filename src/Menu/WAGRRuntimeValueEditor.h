b593a81cb27d427f703496f6e72fc7a383297743
euoradan <zelihmannradan@gmail.com>
2026-07-17 06:57:51 -0300

    dogfood2: make typed value editor linkage safe for ObjC++ callers

--- src/Menu/WAGRRuntimeValueEditor.h
+++ src/Menu/WAGRRuntimeValueEditor.h
@@ -4,6 +4,10 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 void WAGRPresentRuntimeValueEditor(UIViewController *presenter,
                                    UIView * _Nullable sourceView,
                                    NSString *className,
@@ -13,5 +17,9 @@ void WAGRPresentRuntimeValueEditor(UIViewController *presenter,
                                    NSString *currentDescription,
                                    id _Nullable currentRawValue,
                                    dispatch_block_t _Nullable completion);
+
+#ifdef __cplusplus
+}
+#endif
 
 NS_ASSUME_NONNULL_END
