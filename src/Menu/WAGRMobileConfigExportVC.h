231ed2e7d297056206a4610b1aa7522dc847b557
euoradan <zelihmannradan@gmail.com>
2026-08-21 21:55:26 -0300

    dogfood2: add MobileConfig export controller API

---
+++ src/Menu/WAGRMobileConfigExportVC.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface WAGRMobileConfigExportVC : UITableViewController <UISearchResultsUpdating>
+- (instancetype)initWithUserContext:(id _Nullable)userContext;
+@end
+
+NS_ASSUME_NONNULL_END
